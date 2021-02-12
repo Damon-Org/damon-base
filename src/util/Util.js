ec6ea9db382c82cc94ff9b6ee2f19b788d41456d
Yimura <andreas.maerten@scarlet.be>
2021-02-12 13:27:43 +0100

    fix(Util): Call to undefined method of Array
    fixes #3

--- src/util/Util.js
+++ src/util/Util.js
@@ -22,7 +22,7 @@ export const flatten = (obj, ...props) => {
         const valueOf = elemIsObj && typeof element.valueOf === 'function' ? element.valueOf() : null;
 
         // If it's an array, flatten each element
-        if (Array.isArray(element)) out[newProp] = element.map(e => Array.flatten(e));
+        if (Array.isArray(element)) out[newProp] = element.map(e => flatten(e));
         // If it's an object with a primitive `valueOf`, use that value
         else if (typeof valueOf !== 'object') out[newProp] = valueOf;
         // If it's a primitive
